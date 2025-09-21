e7abc6cd48470c178a824bdf00cab6f25a6f4331
Julian <juliandcobo@gmail.com>
2025-09-20 23:00:46 -0400

    Counter now provides pretty-printed report

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ def get_book_text(path_book):
         file_contents = f.read()
         return file_contents
 
+template = """============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found 75767 total words
+--------- Character Count -------"""
+
+
 def main():
     book_text = get_book_text(path_book)
     num_words(book_text)
     d = count_char(book_text)
     the_list = dict_sort(d)
-    print(the_list)
+    print(template)
+    for char in the_list:
+        print(f"{char['char']}: {char['num']}")
 main()
 
--- stats.py
+++ stats.py
@@ -5,18 +5,21 @@ def num_words(file_contents):
         num_words += 1
     print(f"{num_words} words found in the document")
 
-def sort_on(items):
-    return items[1]
+def sort_on(dict):
+    return dict["num"]
 
 def count_char(file_contents):
     book_lower = file_contents.lower()
     d = dict()
     for char in book_lower:
-        d.setdefault(char, 0)
-        d[char] += 1
+        if char.isalpha():
+            d.setdefault(char, 0)
+            d[char] += 1
+        else:
+            pass
     return d
 
 def dict_sort(dictionary):
-    my_list = list(dictionary.items())
-    my_list.sort(reverse = True, key = sort_on)
-    return my_list
+    char_list = [{"char": char, "num": count} for char, count in dictionary.items()]
+    char_list.sort(reverse = True, key = sort_on)
+    return char_list 
